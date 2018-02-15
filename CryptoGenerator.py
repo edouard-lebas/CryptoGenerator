31244dfac085019cc0605063d579203c069cc273
edouard-lebas <lebas.edouard@elebas.fr>
2018-02-15 21:02:12 +0100

    Sha 384

--- CryptoGenerator.py
+++ CryptoGenerator.py
@@ -12,7 +12,8 @@ def menuSha():
     print "2 > SHA224"
     print "3 > SHA256"
     print "4 > SHA512"
-    print "5 > RETURN"
+    print "5 > SHA512"
+    print "6 > RETURN"
     return raw_input("Choice :")
 
 while exit != '1':
@@ -36,10 +37,14 @@ while exit != '1':
             s = txtToShaClass(sha256)
             print s.processSha256()
         elif choiceSha == '4':
+            SHA384 = raw_input("Text To SHA384 : ")
+            s = txtToShaClass(sha384)
+            print s.processSha384()
+        elif choiceSha == '5':
             SHA512 = raw_input("Text To SHA512 : ")
             s = txtToShaClass(sha512)
             print s.processSha512()
-        elif choiceSha == '5':
+        elif choiceSha == '6':
             print "Return"
         else:
             print "Choice Error"
