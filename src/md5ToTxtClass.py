e471235974168288d98049c9e73cf0191b84c3b3
edouard-lebas <lebas.edouard@elebas.fr>
2018-03-18 17:57:10 +0100

    sha to txt

--- src/md5ToTxtClass.py
+++ src/md5ToTxtClass.py
@@ -5,10 +5,10 @@ class md5ToTxtClass:
     def __init__(self, hash):
         self.hash = hash
         self.type = "md5"
-        self.email = ""
-        self.code = ""
+        self.email = "lebas.edouard@elebas.fr"
+        self.code = "7baef180cfe77097"
 
-    def processTxt(self):
+    def processMd5(self):
         params = urllib.urlencode({'hash': self.hash, 'hash_type': self.type, 'email': self.email, 'code': self.code})
         f = urllib.urlopen("http://md5decrypt.net/Api/api.php?%s" % params)
         resp = f.read()
