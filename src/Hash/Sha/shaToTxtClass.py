b5aa862aef5e827e81a0f8b32608251e7871e3ab
edouard-lebas <lebas.edouard@elebas.fr>
2018-04-14 16:54:36 +0200

    sha

---
+++ src/Hash/Sha/shaToTxtClass.py
@@ -0,0 +1,45 @@
+import urllib
+class shaToTxtClass:
+    
+    def __init__(self, hash):
+        self.hash = hash
+        self.type = "sha1"
+        self.email = ""
+        self.code = ""
+
+    def processSha1(self):
+        self.type = "sha1"
+        params = urllib.urlencode({'hash': self.hash, 'hash_type': self.type, 'email': self.email, 'code': self.code})
+        f = urllib.urlopen("http://md5decrypt.net/Api/api.php?%s" % params)
+        resp = f.read()
+        return resp.strip()
+
+    # def processSha224(self):
+    #     self.type = "sha224"
+    #     params = urllib.urlencode({'hash': self.hash, 'hash_type': self.type, 'email': self.email, 'code': self.code})
+    #     f = urllib.urlopen("http://md5decrypt.net/Api/api.php?%s" % params)
+    #     resp = f.read()
+    #     return resp.strip()
+
+    def processSha256(self):
+        self.type = "sha256"
+        params = urllib.urlencode({'hash': self.hash, 'hash_type': self.type, 'email': self.email, 'code': self.code})
+        f = urllib.urlopen("http://md5decrypt.net/Api/api.php?%s" % params)
+        resp = f.read()
+        return resp.strip()
+
+
+    def processSha384(self):
+        self.type = "sha384"
+        params = urllib.urlencode({'hash': self.hash, 'hash_type': self.type, 'email': self.email, 'code': self.code})
+        f = urllib.urlopen("http://md5decrypt.net/Api/api.php?%s" % params)
+        resp = f.read()
+        return resp.strip()
+
+
+    def processSha512(self):
+        self.type = "sha512"
+        params = urllib.urlencode({'hash': self.hash, 'hash_type': self.type, 'email': self.email, 'code': self.code})
+        f = urllib.urlopen("http://md5decrypt.net/Api/api.php?%s" % params)
+        resp = f.read()
+        return resp.strip()
